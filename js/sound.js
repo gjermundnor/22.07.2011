059d0a5d08fae997e30c8fddff31a3c117fbc17d
hannahsollund <hannahlizj@gmail.com>
2016-02-10 14:43:48 +0100

    ferdig med lyd

--- js/sound.js
+++ js/sound.js
@@ -3,14 +3,14 @@ $(document).ready(function(){
     var volumeControlBtn = $("#volumeControlBtn");
     var rainSound = $("#rainSound");
 
-    $("#rainSound").prop("volume", 0.2);
+    rainSound.prop("volume", 0.2);
     
-    $("#volumeControlBtn").click(function(){    
-        decreaseVolume();
+    volumeControlBtn.click(function(){    
+        rainSound.prop("volume", 0.6);
     });
 
-    var decreaseVolume = function(){    
-        $("#rainSound").prop("volume", 0.6);
-    };
+    volumeControlTwoBtn.click(function(){    
+        rainSound.prop("volume", 1);
+    });
 
 });
