56455629117d0a97f56e5d1db4c9df8e9cf1a561
Mats Østvig <mats.ost87@gmail.com>
2016-02-13 12:38:16 +0100

    knapper med tekst

--- js/timeline.js
+++ js/timeline.js
@@ -15,12 +15,7 @@ var TIMELINEAPP = {
     $dot4: null,
     $dot5: null,
     $dot6: null,
-    $dot1Txt: null,
-    $dot2Txt: null,
-    $dot3Txt: null,
-    $dot4Txt: null,
-    $dot5Txt: null,
-    $dot6Txt: null,
+    $text: null,
 
     init: function(){
         TIMELINEAPP.clockEvent();
@@ -41,25 +36,17 @@ var TIMELINEAPP = {
             TA.$dot4 = $("#dot4");
             TA.$dot5 = $("#dot5");
             TA.$dot6 = $("#dot6");
-            TA.$dot1Txt = $("#dot1Txt");
-            TA.$dot1Txt = $("#dot2Txt");
-            TA.$dot1Txt = $("#dot3Txt");
-            TA.$dot1Txt = $("#dot4Txt");
-            TA.$dot1Txt = $("#dot5Txt");
-            TA.$dot1Txt = $("#dot6Txt");
+            TA.$text = $(".text");
+            
         }();
         var setEvents = function () {
-            TA.$dot1.mouseover(TA.dot1Animate);
-            TA.$dot2.mouseover(TA.dot2Animate);
-            TA.$dot3.mouseover(TA.dot3Animate);
-            TA.$dot4.mouseover(TA.dot4Animate);
-            TA.$dot5.mouseover(TA.dot5Animate);
-            TA.$dot6.mouseover(TA.dot6Animate);
+
+            TIMELINEAPP.dotAnimate();
 
         }(); //End setEvents
 
         var clickEvent = function(){
-
+            
           $('#addNameBtn').click( TIMELINEAPP.questionOne );
           $('#goNextBtn').click( TIMELINEAPP.questionTwo );
           $('#goNextInfoBtn').click( TIMELINEAPP.questionThree );
@@ -67,35 +54,21 @@ var TIMELINEAPP = {
         }();
     }, // END init
 
-    dot1Animate: function () {
+    dotAnimate: function () {
 
-        TIMELINEAPP.$dot1Txt.html("Tekst");
-
+        $(".dot").hover(function(){
+           $(this)
+               .siblings(".text")
+                .stop()
+               .animate({"opacity": "1"});
+        }, function(){
+            $(this)
+               .siblings(".text")
+                .stop()
+               .animate({"opacity": "0"});
+        });
     },
-    dot2Animate: function () {
 
-        TIMELINEAPP.$dot2Txt.html("Tekst");
-
-    },
-    dot3Animate: function () {
-
-         TIMELINEAPP.$dot3Txt.html("Tekst");
-
-    },
-    dot4Animate: function () {
-
-          TIMELINEAPP.$dot4Txt.html("Tekst");
-
-    },
-    dot5Animate: function () {
-          TIMELINEAPP.$dot5Txt.html("Tekst");
-
-    },
-    dot6Animate: function () {
-
-        TIMELINEAPP.$dot6Txt.html("Tekst");
-
-    },
 
     clockEvent: function(){
           var scrollValue = 464.9; // Må legge til høyden fra toppen til der tiden skal begynne
